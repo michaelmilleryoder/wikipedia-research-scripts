da8def4952e46e064dbb2f6b4c509c54fbeab13a
Michael Miller <michaelalanmiller@gmail.com>
2017-04-17 13:38:35 -0400

    Updated score_editors

--- score_editors.py
+++ score_editors.py
@@ -2,10 +2,9 @@ import pandas as pd
 import csv
 from string import ascii_letters
 from nltk.corpus import words, stopwords
-import nltk
 from collections import Counter, defaultdict
 import re, hashlib, os
-from pandas.tseries.offsets import *
+from pandas.tseries.offsets import DateOffset
 import numpy as np
 import math
 from tqdm import tqdm
@@ -19,7 +18,7 @@ Input:
 * Directory with article revision histories TSV files, 1 per row
 
 Output:
-* CSV with all article edits scored (edscores_path)
+* CSV of multiple CSVs with all article edits scored (edscores_path)
 * CSV with all talk page contributions in a conversation for an editor, 1 per 
 line, scored (outpath)
 
@@ -27,96 +26,16 @@ line, scored (outpath)
 
 """ I/O vars """
 # input
-infile = '/home/michael/school/research/wp/ar/artalk_cspages_segmented.csv'
-diff_dir = '/home/michael/school/research/wp/ar/ar_article_diffs/'
-ardict_path = '/home/michael/school/research/wp/ar/arabic-wordlist-1.6.txt'
+infile = 'enwiki_talk/talk_filtered_2.csv'
+diff_dir = 'enwiki_diffs/'
+#ardict_path = '/home/michael/school/research/wp/ar/arabic-wordlist-1.6.txt'
 
 # output
-edscores_path = '/home/michael/school/research/wp/ar/editor_thread_scores.csv'
-outpath = '/home/michael/school/research/wp/ar/cs_talk_scores.csv'
+edscores_path = 'enwiki_edit_scores/'
+outpath = 'enwiki_talk/enwiki_talk_scores.csv'
 
-
-""" Check for conversations with reverts """
-
-def check_reverts():
-
-    # Load talk data
-    cs_threads = pd.read_csv(infile, parse_dates=['timestamp'])
-
-    # Organize into threads
-    threadtimes = {} # (article_title, thread_title): [beg_timestamp, end_timestamp, [users]]
-
-    threads = set(zip(cs_threads['article_title'], cs_threads['thread_title']))
-
-    # Collect thread info
-    for art_name, t in threads:
-        rows = cs_threads[(cs_threads['article_title']==art_name) & (cs_threads['thread_title']==t)]
-        beg = min(rows['timestamp'])
-        end = max(rows['timestamp'])
-        users = set(rows['username'].tolist())
-        threadtimes[(art_name, t)] = [beg, end, users]
-
-    del cs_threads
-
-
-    # Get threads with reverts in timeframe
-    rev_dirpath = '/home/michael/school/research/wp/ar/ar_articles/with_reverts'
-    n_matches = 0
-    n_posts = 0
-    match_threads = []
-
-    for i, (art_name, t) in enumerate(threadtimes):
-    #     print(art_name)
-        
-        beg = threadtimes[art_name, t][0] - DateOffset(days=7)
-        end = threadtimes[art_name, t][1] + DateOffset(days=7)
-        users = threadtimes[art_name, t][2]
-        
-        if art_name.startswith('Discussion:'):
-            art_name = art_name[len('Discussion:'):]
-        
-        # Load revisions
-        rev_path = os.path.join(rev_dirpath, art_name + '.csv')
-        if os.path.isfile(rev_path):
-            print(n_matches, end="/")
-            print(i)
-            chunks = pd.read_csv(rev_path, parse_dates=[1], header=None, iterator=True, chunksize=1000)
-            revs = pd.concat(chunks, ignore_index=True)
-            del chunks
-            reverts = revs[revs[5] == 1]
-        
-            time_mask = [ts >= beg and ts <= end for ts in reverts[1]]
-            selected_rows = reverts[time_mask]
-            if not selected_rows.empty:
-                selected_rows = selected_rows[selected_rows[3].isin(users)]
-            if not selected_rows.empty:
-                # Check if reverted also in discussion
-                sel_posts = 0 # actually are article edits, not posts
-                for ind in selected_rows.index: 
-                    if revs.loc[ind-1, 3] in users:
-                        sel_posts += 1
-                n_posts += sel_posts
-                if sel_posts > 0:
-                    n_matches += 1
-                    match_threads.append((art_name, t))
-                    
-            del revs
-
-    print('Number of threads with reverts: {:d}'.format(n_matches))
-
-
-    # Make csv of discussion posts
-    cs_threads = pd.read_csv(infile, parse_dates=['timestamp'])
-    rev_threads_inds = []
-    for art_name, t in match_threads:
-        rows = cs_threads[(cs_threads['article_title']==art_name) & (cs_threads['thread_title']==t)]
-        rev_threads_inds.extend(rows.index)
-
-    # Will want to remove duplicates, which come from the original cs_threads having 'Discussion' or not
-    cs_revert_talk = cs_threads.loc[rev_threads_inds, :].sort_index()
-    cs_revert_talk.to_csv(rv_outpath, index=None)
-    print('Wrote revert info')
-
+# settings
+mult_files = True # whether or not want edscores to be printed to multiple files (if will be scoring lots of edits) or a single file
 
 
 def dict_diff(orig, chg):
@@ -142,55 +61,58 @@ def dict_diff(orig, chg):
             
     return max(1-chg_abs_sum/orig_abs_sum, 0.0), orig_abs_sum
 
-def score_editors():
+
+
+def score_editors(stops, talk):
     """ Score editors
         Print out article edit file with score for each edit 
+
+        Args:
+            stops: list of stopwords
+            talk: DataFrame of talk data
     """
 
     print("Scoring editors ...")
 
     # Load, initialize data
-    talk = pd.read_csv(infile, parse_dates=['timestamp'])
     assert os.path.exists(diff_dir)
     thread_durs = []
 
     # SCORE THREAD-WISE WINNERS FOR EACH DISCUSSION AND EDIT PARTICIPANT
-    art_data = pd.DataFrame() # Final article edit spreadsheet with scores, will be organized by article edit keys
-    art_data['edit_timestamp'] = pd.Series()
-    art_data['edit_score'] = pd.Series()
-    art_data['editor_thread_score'] = pd.Series()
-    art_data['additions'] = pd.Series()
-    art_data['deletions'] = pd.Series()
-    art_data['comparison_timestamp'] = pd.Series()
+    art_data = [] # Final article edit spreadsheet with scores, will be organized by article edit keys
+    art_data_colnames = ['article', 'edit_timestamp', 
+                        'additions', 'deletions',
+                        'editor', 'edit_comment',
+                         'edit_score', 'thread_title',
+                        'comparison_timestamp',
+                        'editor_thread_score']
 
-    threads = sorted(list(set(zip(talk['article_title'], talk['thread_title']))))
+    threads = sorted(list(set(zip(talk['article'], talk['thread']))))
 
-    stops = stopwords.words('arabic') 
     prev_art = ''
 
     # Get edits by thread editors in between initial revert and session end
     # session end: end of thread or last revert 7 days after last thread entry by thread participants
-    art_data = pd.DataFrame()
 
     no_article_ctr = 0
-    #for i, (art, thread) in enumerate(threads):
-    for i, (art, thread) in enumerate(tqdm(threads)):
+    for i, (art, thread) in enumerate(tqdm(threads[:30])):
         
         # Talk page participants
-        talk_parts = set(talk[(talk['article_title']==art) & (talk['thread_title']==thread)]['username'].values)
+        talk_parts = set(talk[(talk['article']==art) & (talk['thread']==thread)]['username'].values)
         
-        talk_rows = talk[(talk['article_title'] == art) 
-                                & (talk['thread_title'] == thread)
-                                ].loc[:, ['article_title', 'thread_title', 'post_timestamp']]
+        talk_rows = talk[(talk['article'] == art) 
+                                & (talk['thread'] == thread)
+                                ].loc[:, ['article', 'thread', 'timestamp']]
         thread_end = max(talk['timestamp'])
         thread_beg = min(talk['timestamp'])
         
         # Build edit history from beg to end of thread
         if prev_art != art:
-            artfp = os.path.join(diff_dir, art.replace(' ', '_').replace('/', '_') + '_diff.csv')
+            artfp = os.path.join(diff_dir, art.replace(' ', '_').replace('/', '_').lower() + '_diff.csv')
             if not os.path.exists(artfp):
                 no_article_ctr += 1
                 tqdm.write("{:d} No article".format(no_article_ctr))
+                pdb.set_trace()
                 continue
             diff_data = pd.read_csv(artfp, parse_dates=['timestamp'])
         sess_beg = thread_beg - DateOffset(days=1)
@@ -210,9 +132,12 @@ def score_editors():
         edscores = defaultdict(lambda: [0,0]) # [n_wds_successful, n_wds_changed]
         
         # Calculate success score for each edit compared with end revision
+        new_rows = []
         for row in sess_edits.itertuples():
-            edit_text = diff_data.loc[diff_data['timestamp']==row.timestamp]
-            diffs = diff_data.loc[(diff_data['timestamp'] > row.timestamp) & 
+            row_timestamp = row[2]
+            row_editor = row[5]
+            edit_text = diff_data.loc[diff_data['timestamp']==row_timestamp]
+            diffs = diff_data.loc[(diff_data['timestamp'] > row_timestamp) & 
                 (diff_data['timestamp'] <= (sess_end + DateOffset(days=1)))] 
                 # includes edits by non-talk participants, as should
             
@@ -232,8 +157,8 @@ def score_editors():
             if diffs.empty: # No revisions after thread end
                 edit_score = 1.0
                 n_wds = abs(sum(edit_diff.values()))
-                edscores[row.editor][0] += edit_score * n_wds
-                edscores[row.editor][1] += n_wds
+                edscores[row_editor][0] += edit_score * n_wds
+                edscores[row_editor][1] += n_wds
                     
             else: 
                 
@@ -247,37 +172,53 @@ def score_editors():
                 changes.update(next_addwds)
                 
                 edit_score, n_wds = dict_diff(edit_diff, changes)
-                edscores[row.editor][0] += edit_score * n_wds
-                edscores[row.editor][1] += n_wds
+                edscores[row_editor][0] += edit_score * n_wds
+                edscores[row_editor][1] += n_wds
 
-            new_row = pd.DataFrame([row]) # SLOW, OPTIMIZE
-            new_row['edit_score'] = edit_score
-    #             new_row['winner'] = winner
-            new_row['thread_title'] = thread
-            if not diffs.empty:
-                new_row['comparison_timestamp'] = max(diffs['timestamp'])
-            new_row.rename(columns={'timestamp': 'edit_timestamp', 'article_name': 'article'}, inplace=True)
-            new_row.drop('Index', axis=1, inplace=True)
-            art_data = art_data.append(new_row, ignore_index=True)
-                
-        art_data.reset_index(drop=True)
+            new_row = list(row[1:]) # don't want index
+            new_row.append(edit_score)
+            new_row.append(thread) # thread_title
+            if diffs.empty:
+                new_row.append(np.nan)
+            else:
+                new_row.append(max(diffs['timestamp'])) # comparison_timestamp
+            #art_data.append(new_row)
+            new_rows.append(new_row)
+
         prev_art = art
         
         # Calculate editor thread scores
+#        art_data['editor_thread_score'] = pd.Series()
+#        for ed in sess_parts:
+#            sess_finalrows = art_data[(art_data['article']==art) & 
+#                            (art_data['thread_title']==thread) &
+#                            (art_data['editor']==ed)]
+#            if edscores[ed][1] == 0: # editor whose only edit was the final one and was of no words
+#                ed_threadscore = 1.0
+#            else:
+#                ed_threadscore = edscores[ed][0]/edscores[ed][1]
+#            if ed_threadscore > 1.0 or ed_threadscore < 0.0:
+#                debug_here()
+#            for idx in sess_finalrows.index:
+#                art_data.loc[idx, 'editor_thread_score'] = ed_threadscore
+        
         for ed in sess_parts:
-            sess_finalrows = art_data[(art_data['article']==art) & 
-                            (art_data['thread_title']==thread) &
-                            (art_data['editor']==ed)]
+            sess_finalrows = [r for r in new_rows \
+                                if r[0]==art and r[-2]==thread and \
+                                r[4]==ed]
             if edscores[ed][1] == 0: # editor whose only edit was the final one and was of no words
                 ed_threadscore = 1.0
             else:
                 ed_threadscore = edscores[ed][0]/edscores[ed][1]
             if ed_threadscore > 1.0 or ed_threadscore < 0.0:
-                debug_here()
-            for idx in sess_finalrows.index:
-                art_data.loc[idx, 'editor_thread_score'] = ed_threadscore
+                pdb.set_trace()
+            for i in range(len(sess_finalrows)):
+                sess_finalrows[i].append(ed_threadscore)
         
+            art_data.extend(sess_finalrows)
+
     # Sort art_data
+    art_data = pd.DataFrame(art_data, columns=art_data_colnames)
     art_data.sort_values(['article', 'thread_title', 'edit_timestamp'], inplace=True)
 
     # Select columns
@@ -290,19 +231,35 @@ def score_editors():
     art_data = art_data[mask]
         
     art_data.drop_duplicates(inplace=True)
-    art_data.to_csv(edscores_path, index=False)
-    len(art_data)
+
+    # Print edit scores
+    if mult_files:
+        art_data.to_csv(os.path.join(edscores_path, art.replace(' ', '_').replace('/', '_').lower() + '_edit_scores.csv'), index=False)
+    else:
+        art_data.to_csv(edscores_path, index=False)
     print('Wrote edscores to {:s}'.format(edscores_path))
 
-def build_out():
-    """ Assemble editor scores with talk page discussion """
-    art_data = pd.read_csv(edscores_path)
-    talk = pd.read_csv(infile, parse_dates=['timestamp'])
+def build_out(talk):
+    """ Assemble editor scores with talk page discussion 
+        Args:
+            talk: DataFrame of talk
+    """
+
+    if not mult_files:
+        art_data = pd.read_csv(edscores_path)
+
+        edthreads = sorted(set((zip(art_data['article'], art_data['thread_title'], art_data['editor'], art_data['editor_thread_score']))))
+
+    else:
+        edthreads = []
+        for fname in os.listdir(edscores_path):
+            art_data = pd.read_csv(os.path.join(edscores_path, fname))
+            ind_edthreads = set((zip(art_data['article'], art_data['thread_title'], art_data['editor'], art_data['editor_thread_score'])))
+            edthreads.extend(list(ind_edthreads))
+
+        edthreads = sorted(edthreads)
 
     # Build input corpora of editors' text + others' text, get labels
-    edthreads = sorted(set((zip(art_data['article'], art_data['thread_title'], art_data['editor'], art_data['editor_thread_score']))))
-    edthreads
-
     # Assemble input text of just editors' text
     edtalk = defaultdict(str)
     n_edturns = {}
@@ -318,25 +275,26 @@ def build_out():
             #len(usernames.intersection(endict))/len(usernames)))
 
     # Check usernames against an Arabic dictionary
-    with open(ardict_path) as f:
-        ardict = set([w for w in f.read().splitlines()])
+    #with open(ardict_path) as f:
+        #ardict = set([w for w in f.read().splitlines()])
     #print("Username overlap with Arabic dictionary: {:.1%}".format(
     #        len(usernames.intersection(ardict))/len(usernames)))
 
     # Filter out dict words from usernames
-    usernames = set([u for u in usernames if not u in ardict and not u in endict])
+    #usernames = set([u for u in usernames if not u in ardict and not u in endict])
+    usernames = set([u for u in usernames if not u in endict])
 
     print("Assembling editor and other text ...")
     for i, el in enumerate(tqdm(edthreads)):
-        rows = talk[(talk['article_title']==el[0]) &
-                        (talk['thread_title']==el[1])]
+        rows = talk[(talk['article']==el[0]) &
+                        (talk['thread']==el[1])]
 
         # Editor rows
         edrows = rows[rows['username']==el[2]]
 
-        # Screen out usernames        
+        # Screen out usernames--TODO separate preprocess fn like for ar
         screened = []
-        for t in edrows['post_text'].tolist():
+        for t in edrows['text'].tolist():
             new_t = ' '.join([w for w in str(t).split() if not w in usernames and not '--' in w]) 
                 # handle --username, doesn't handle multi-word usernames
             screened.append(new_t)
@@ -349,7 +307,7 @@ def build_out():
 
         # Screen out usernames        
         screened = []
-        for t in other_rows['post_text'].tolist():
+        for t in other_rows['text'].tolist():
             new_t = ' '.join([w for w in str(t).split() if not w in usernames and not '--' in w])
             screened.append(new_t)
 
@@ -373,7 +331,7 @@ def build_out():
 
 def main():
 
-    #talk = pd.read_csv(infile, parse_dates=['timestamp'])
+    talk = pd.read_csv(infile, parse_dates=['timestamp'])
 
     ## Filter out usernames from text
     #usernames = set(talk['username'].tolist())
@@ -389,8 +347,12 @@ def main():
     #print("Username overlap with Arabic dictionary: {:.1%}".format(
     #        len(usernames.intersection(ardict))/len(usernames)))
 
-    #score_editors()
-    build_out()
+    #stops = stopwords.words('arabic') 
+    with open("stopwords_en.txt") as f:
+        stops = f.read().splitlines()
+
+    score_editors(stops, talk)
+    build_out(talk)
 
 if __name__ == '__main__':
     main()
